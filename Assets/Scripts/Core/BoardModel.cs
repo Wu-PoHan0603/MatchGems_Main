4cb68b8dfe6228d113efae3263bf0263c8b7d75c
Wu-PoHan0603 <e124685026@gmail.com>
2026-06-29 08:41:38 +0800

    Class03 棋盤陣列與寶石資料

--- Assets/Scripts/Core/BoardModel.cs
+++ Assets/Scripts/Core/BoardModel.cs
@@ -5,6 +5,54 @@ namespace MatchGem.Core
     /// </summary>
     public class BoardModel
     {
+        #region #基本參數
+        private GemData[,] _gems; // [,]二維陣列
+        #endregion #基本參數
 
+        #region #公開參數接口
+        /// <summary>
+        /// 棋盤的寬度
+        /// </summary>
+        public int Width { get; }
+        /// <summary>
+        /// 棋盤的高度
+        /// </summary>
+        public int Height { get; }
+        #endregion #公開參數接口
+
+        #region #建構式
+        /// <summary>
+        /// 建立指定尺寸的棋盤
+        /// </summary>
+        /// <param name="w">寬</param>
+        /// <param name="h">高</param>
+        public BoardModel(int w, int h)
+        {
+            Width = w;
+            Height = h;
+            _gems = new GemData[Width,Height];
+        }
+        #endregion #建構式
+
+        #region 公開方法
+        /// <summary>
+        /// 設定指定格子的寶石
+        /// </summary>
+        /// <param name="coord">定位資料</param>
+        /// <param name="gemType">寶石類型</param>
+        public void SetGem(CellCoord coord, GemType gemType)
+        {
+            _gems[coord.X,coord.Y] = new GemData(gemType);
+        }
+        /// <summary>
+        /// 取得指定格子的寶石
+        /// </summary>
+        /// <param name="coord">定位資料</param>
+        /// <returns>寶石資料</returns>
+        public GemData GetGem(CellCoord coord)
+        {
+            return _gems[coord.X,coord.Y];//有設定就要有拿
+        }
+        #endregion 公開方法
     }
 }
