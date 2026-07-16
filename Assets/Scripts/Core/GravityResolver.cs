6b70d5029e6b05983bf4c74386fb7c50a0a2d56d
wupohan0603 <b6x4kvc78q@privaterelay.appleid.com>
2026-07-16 19:32:21 +0800

    Class12 新-重力計算功能(KeyGem鋪路)

--- Assets/Scripts/Core/GravityResolver.cs
+++ Assets/Scripts/Core/GravityResolver.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MatchGems.Core
@@ -7,13 +8,57 @@ namespace MatchGems.Core
     /// </summary>
     public class GravityResolver
     {
+        /// <summary>
+        /// 重力移動暫存清單
+        /// </summary>
+        private readonly List<TileMove> moves = new List<TileMove>();
+        /// <summary>
+        /// 定位用座標暫存
+        /// </summary>
+        private readonly CellCoord coordFrom = new CellCoord();
+        private readonly CellCoord coordTo = new CellCoord();
         #region 公開方法
-        public void Resolve(BoardModel board)
+        /// <summary>
+        /// 原始墜落方式
+        /// </summary>
+        /// <param name="board"></param>
+        /*public void Resolve(BoardModel board)
         {
             for (int x = 0; x < board.Width; x++)
             {
                 DropColumn(board, x);
             }
+        }*/
+        /// <summary>
+        /// PlanB：回傳每顆寶石 From → TO 紀錄
+        /// </summary>
+        /// <param name="board"></param>
+        /// <returns>移動記錄清單</returns>
+        public List<TileMove> Resolve(BoardModel board)
+        {
+            moves.Clear();
+
+            for (int x = 0; x < board.Width; x++)
+            {
+                int writeY = 0;//Y定位
+                for (int ready = 0; ready < board.Width; ready++)
+                {
+                    coordFrom.Set(x, ready);
+                    if (!board.HasGem(coordFrom)) continue;
+
+                    coordTo.Set(x, writeY);
+                    if(ready > writeY)
+                    {
+                        board.SetGem(coordTo, board.GetGem(coordFrom));
+                        board.ClearGem(coordFrom);
+                    }
+
+                    moves.Add(new TileMove(coordFrom, coordTo));
+                    writeY++;//Y定位增加
+                }
+            }
+
+            return moves;
         }
         #endregion 公開方法
 
